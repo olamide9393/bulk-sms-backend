8a702647fc3bda7c6778df9c6d7c81d76d63b563
alaye2020 <alayengg@gmail.com>
2023-09-27 20:09:15 -0700

    first commit

--- routes/Message.js
+++ routes/Message.js
@@ -4,20 +4,8 @@ const VarifyToken = require("../middleware/VarifyToken");
 // const Message = require("../model/Message");
 
 const router = express.Router();
-// router.get("/:id", getUserHistory);
-// Define a route to fetch a user's message history
-// router.get('/getUserHistory', VarifyToken, async (req, res) => {
-//          try {
-//            const userHistory = await Message.find({ sender: req.user }).exec();
-//            res.json(userHistory);
-//          //   console.log(userHistory);
-//          } catch (error) {
-//                   res.status(404).json({mes: 'no history'})
-//            res.status(500).json({ error: 'Internal server error' });
-//          }
-//        });
        
 router.get("/getUserHistory",VarifyToken, getUserHistory);
-router.post("/message",VarifyToken, message);
+router.post("/message", message);
 
 module.exports = router;
